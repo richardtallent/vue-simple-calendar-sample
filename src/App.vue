94dc86e80fc0dbb36fc1c4bc6dbf2bb9492f3a47
Richard Tallent <richardtallent@Richard-MacMini.local>
2020-11-15 14:54:42 -0600

    Split example to show array and string variants

--- src/App.vue
+++ src/App.vue
@@ -285,14 +285,14 @@ export default {
 		myDateClasses() {
 			// This was added to demonstrate the dateClasses prop. Note in particular that the
 			// keys of the object are `yyyy-mm-dd` ISO date strings (not dates), and the values
-			// for those keys are string **arrays**. Keep in mind that your CSS to style these
+			// for those keys are strings or string arrays. Keep in mind that your CSS to style these
 			// may need to be fairly specific to make it override your theme's styles. See the
 			// CSS at the bottom of this component to see how these are styled.
 			const o = {}
 			const theFirst = this.thisMonth(1)
 			const ides = [2, 4, 6, 9].includes(theFirst.getMonth()) ? 15 : 13
 			const idesDate = this.thisMonth(ides)
-			o[this.isoYearMonthDay(idesDate)] = ["ides"]
+			o[this.isoYearMonthDay(idesDate)] = "ides"
 			o[this.isoYearMonthDay(this.thisMonth(21))] = [
 				"do-you-remember",
 				"the-21st",
